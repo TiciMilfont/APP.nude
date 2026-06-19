4221756cace4fe282cd33aca6b08d99165b92686
TiciMilfont <ticioanmilfont@gmail.com>
2026-06-19 17:04:07 -0300

    Primeiro commit do projeto

---
+++ Lanchy/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./COMPONENTS/HEADER";
+import Login from "./COMPONENTS/login";
+
+
+function App(){
+  return(
+
+    <>
+
+    <Header />  {/* aqui famos fazer os pedacinhos*/} 
+    <Login />
+    
+    </>
+
+  )
+
+}
+
+export default App 
---
+++ Lanchy/src/COMPONENTS/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+<>
+        <h1> LOGIN </h1>
+        <h2> Entre aqui: </h2>
+
+</>
+    )
+}
+
+export default Login
