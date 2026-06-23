c23f1a4237c3ef3e49b59b207d1f140be1aeb063
TiciMilfont <ticionamilfont@gmail.com>
2026-06-23 16:11:24 -0300

    Segundo commit- propridades no Header

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./COMPONENTS/HEADER";
+import Login from "./COMPONENTS/login";
+
+
+function App(){
+  return(
+
+    <>
+
+    < Header titulo ="LANCHYS"   /* aqui famos fazer os pedacinhos*/
+      subtitulo =" the best hot-dog" /> 
+    <Login />
+    
+    </>
+
+  )
+
+}
+
+export default App 
---
+++ src/COMPONENTS/HEADER.jsx
@@ -0,0 +1,12 @@
+function Header({titulo, subtitulo}) {
+    return (
+<>
+        <h1> {titulo} </h1>
+        <h2> {subtitulo} </h2>
+       
+
+</>
+    )
+}
+
+export default Header
